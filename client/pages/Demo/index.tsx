a2eb16882029b471b0e495afc0d4973fd0232b5a
yunkui.zhou <yunkui.zhou@bkjk.com>
2018-12-21 19:13:35 +0800

    feature:升级glue-redux

--- client/pages/Demo/index.tsx
+++ client/pages/Demo/index.tsx
@@ -8,27 +8,36 @@ interface Person {
 }
 interface Props {
   person: Person;
+  country: string;
 }
 class DemoClass extends PureComponent<Props> {
-  ref: RefObject<any>;
+  refName: RefObject<any>;
+  refCountry: RefObject<any>;
   constructor(props: Props) {
     super(props);
-    this.ref = React.createRef();
+    this.refName = React.createRef();
+    this.refCountry = React.createRef();
   }
 
   onClick = async () => {
-    const { value } = this.ref.current;
+    const { value } = this.refName.current;
     demoModel.person({title: value});
+    const { value: country } = this.refCountry.current;
+    demoModel.country(country);
   }
 
   render() {
-    const { person } = this.props;
+    const { person, country } = this.props;
     return (
       <div className="demo-container">
         <form action="/" method="get">
           <label htmlFor="input">
-              输入任意内容：
-            <input ref={this.ref} type="text" id="input" />
+              人名：
+            <input ref={this.refName} type="text" id="input" />
+          </label>
+          <label htmlFor="country">
+            国家：
+            <input ref={this.refCountry} type="text" id="country"/>
           </label>
           <button type="button" onClick={this.onClick}>
               提交
@@ -37,6 +46,9 @@ class DemoClass extends PureComponent<Props> {
         <p>
             添加的人名为：
           {person.title}
+          <br/>
+            添加的国家为：
+          {country}
         </p>
       </div>
     );
--- client/pages/models/demo.ts
+++ client/pages/models/demo.ts
@@ -1,11 +1,13 @@
-import { gluer } from 'glue-redux';
+import { gluer, gas } from 'glue-redux';
 
 const person = gluer((data, state) => ({ ...state, ...data }), {
   title: '',
 });
 
+const country = gas(async (cou) => cou, gluer('China'));
 const demo = {
   person,
+  country
 };
 
 export default demo;
