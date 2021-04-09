3e9ba40db0e6a18d4e7cfc1cc3077f9d63fd63bc
zhouyunkui <zhouyunkui@admindeMacBook-Pro.local>
2021-04-09 19:25:16 +0800

    feat: 例子

---
+++ src/pages/Three/Flow/index.tsx
@@ -0,0 +1,18 @@
+import React, {FC, PropsWithChildren} from 'react';
+
+interface Props {
+
+}
+
+interface Staff {
+  id: number;
+  name: string;
+  from: number;
+  to: number;
+}
+
+const Flow: FC<Props> = (props: PropsWithChildren<Props>) => {
+
+}
+
+export default Flow;
