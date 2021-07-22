e0a292a69460f9a984113b950d061eb5fc9c80b7
zhouyunkui <geasebee@126.com>
2021-07-22 19:22:53 +0800

    mod: 调整依赖

--- src/pages/Demo/Loading/index.tsx
+++ src/pages/Demo/Loading/index.tsx
@@ -1,20 +1,21 @@
 import React, { FC, useCallback } from 'react';
-import { useIndividualModel } from 'femo';
+import { gluer, useIndividualModel, useModel } from 'femo';
 
 interface Props {
 
 }
 
+const nameModel = gluer('测试');
 const Loading: FC<Props> = (_props) => {
   const getCount = useCallback(() => new Promise((resolve) => {
     setTimeout(() => resolve(200), 2000);
   }), []);
 
   const [count,,, { loading }] = useIndividualModel(0, [getCount]);
-
+  const [name] = useModel(nameModel);
   console.log('loading：', loading, ' count：', count);
   return (
-    <span>{count}</span>
+    <span>{count} {name}</span>
   );
 };
 
