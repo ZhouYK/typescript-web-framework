c0afd53bbca461ccad804ecc82ebf6d2e46f7e26
zhouyunkui <zhouyunkui.me@bytedance.com>
2023-06-30 19:08:05 +0800

    feat: 示例调整

--- src/pages/Demo/Form/fields/ArrayField.tsx
+++ src/pages/Demo/Form/fields/ArrayField.tsx
@@ -1,28 +1,46 @@
 import AgeField from '@/pages/Demo/Form/fields/AgeField';
 import NameField from '@/pages/Demo/Form/fields/NameField';
 import Field from '@/pages/Demo/Form/lib/Field';
-import React, { FC } from 'react';
+import { Button } from '@arco-design/web-react';
+import React, { FC, useState } from 'react';
 
 interface Props {
 
 }
 
 const ArrayField: FC<Props> = () => {
-  const arr = [0, 1, 2, 3, 4];
+  const [arr, setArr] = useState(() => {
+    return [0, 1, 2, 3, 4];
+  });
+
+  const onClick = () => {
+    setArr((preArr) => {
+      const tmp = [...preArr];
+      if (preArr.length > 2) {
+        tmp.shift();
+      } else {
+        tmp.unshift(1);
+      }
+      return tmp;
+    });
+  };
 
   return (
-    <Field name='list'>
-      {
-        arr.map((n) => {
-          return (
-            <Field key={n} name={`${n}`}>
-              <NameField name='name' ageFieldPath={['list', `${n}`, 'age']} />
-              <AgeField label='年龄' name='age' nameFieldPath={['list', `${n}`, 'name']} />
-            </Field>
-          );
-        })
-      }
-    </Field>
+    <>
+      <Button onClick={onClick}>随机改变顺序</Button>
+      <Field name='list'>
+        {
+          arr.map((n, index) => {
+            return (
+              <Field key={n} name={`${index}`}>
+                <NameField name='name' ageFieldPath={['list', `${n}`, 'age']} />
+                <AgeField label='年龄' name='age' nameFieldPath={['list', `${n}`, 'name']} />
+              </Field>
+            );
+          })
+        }
+      </Field>
+    </>
   );
 };
 
